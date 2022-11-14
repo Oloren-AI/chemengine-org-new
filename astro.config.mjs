c60de6e37887ec33a6f4c9d840e7ce1ba2cbef35
Raunak Chowdhuri <raunak@oloren.ai>
2022-11-13 23:28:35 -0500

    github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://chemengine.org',
+  site: 'https://oloren-ai.github.io',
+  base: '/chemengine-org-new',
   integrations: [tailwind()]
 });
